331c1157381a19695aa0ccf1d1c7b4cadc867b84
pxzzte <phanxuantue123@gmail.com>
2025-09-26 21:14:44 +0700

    Update a.lua

--- a.lua
+++ a.lua
@@ -2,7 +2,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 local lp = game.Players.LocalPlayer
 
--- Sửa thông báo
+-- Sửa thông báo cho đúng
 local function showNoti(title, text)
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = title,
@@ -11,26 +11,35 @@ local function showNoti(title, text)
     })
 end
 
--- helper teleport
+-- Teleport
 local function tp(cf)
     if lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
         lp.Character.HumanoidRootPart.CFrame = cf
-        showNoti("Teleport", "Dịch chuyển đến part!")
+        showNoti("Teleport", "Đã dịch chuyển!")
     end
 end
 
--- helper invoke - SIÊU ĐƠN GIẢN
+-- Đơn giản hóa collect - chỉ chạy code cơ bản
 local function collect(code)
     if code and code ~= "" then
-        -- Đơn giản là chạy thẳng code
-        loadstring(code)()
-        showNoti("Collect", "Đã chạy code!")
-    else
-        showNoti("Collect", "Không có code!")
+        -- Thử cả loadstring và load
+        local func
+        if loadstring then
+            func = loadstring(code)
+        else
+            func = load(code)
+        end
+        
+        if func then
+            func()
+            showNoti("Collect", "Đã chạy code!")
+        else
+            showNoti("Lỗi", "Không thể load code!")
+        end
     end
 end
 
--- positions (CFrame)
+-- positions
 local positions = {
     CFrame.new(-54.040, 104.454, 469.482, -0.875110269, 9.74059304e-08, -0.483923525, 9.67963274e-08, 1, 2.6240647e-08, 0.483923525, -2.38785596e-08, -0.875110269),
     CFrame.new(-79.5975571, 100.734673, 522.244507, -0.851041913, 3.37455042e-08, -0.525097787, -4.3390993e-09, 1, 7.12976913e-08, 0.525097787, 6.29557775e-08, -0.851041913),
@@ -38,59 +47,49 @@ local positions = {
     CFrame.new(106.348709, 104.733131, 432.794159, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 }
 
--- store user inputs
 local codeStrings = {"", "", "", ""}
 
 -- UI
 local Window = Library:NewWindow("UGC Auto")
 local Tab = Window:NewSection("Main")
 
--- Lobby button
 Tab:CreateButton("Go To Lobby", function()
-    local remote = game:GetService("ReplicatedStorage")
-        :WaitForChild("Packages"):WaitForChild("Knit")
-        :WaitForChild("Services"):WaitForChild("GameService")
-        :WaitForChild("RF"):WaitForChild("GoToLobby")
-    remote:InvokeServer()
-    showNoti("Lobby", "Đã về Lobby!")
+    pcall(function()
+        local remote = game:GetService("ReplicatedStorage").Packages.Knit.Services.GameService.RF.GoToLobby
+        remote:InvokeServer()
+        showNoti("Lobby", "Đã về Lobby!")
+    end)
 end)
 
--- Part textboxes and buttons
 for i=1,4 do
-    Tab:CreateTextbox("Code for Part "..i, "", function(value)
+    Tab:CreateTextbox("Part "..i, "", function(value)
         codeStrings[i] = value
     end)
     
     Tab:CreateButton("Go Part "..i, function()
         tp(positions[i])
-        task.wait(0.5)
+        wait(0.5)
         collect(codeStrings[i])
     end)
 end
 
--- Auto toggle
 local looping = false
-Tab:CreateToggle("Auto Run Parts", function(state)
+Tab:CreateToggle("Auto", function(state)
     looping = state
     if looping then
-        task.spawn(function()
+        spawn(function()
             while looping do
                 for i=1,4 do
                     if not looping then break end
                     tp(positions[i])
-                    task.wait(0.5)
+                    wait(0.5)
                     collect(codeStrings[i])
-                    task.wait(2)
+                    wait(2)
                 end
-                task.wait(0.5)
+                wait(0.5)
             end
         end)
     end
 end)
 
--- Test ngay khi load script
-showNoti("Script", "Đã load script thành công!")
-
--- Test function collect
-task.wait(2)
-collect('showNoti("Test", "Code chạy được!")')
+showNoti("Script", "Loaded!")
